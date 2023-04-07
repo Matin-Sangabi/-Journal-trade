6c6ebcb255644892768eb4ad0c6cab6765826fb5
matinsangabi <sangabiwork@gmail.com>
2023-04-08 01:59:25 +0430

    spot icon and multi toggle button added

---
+++ src/components/icons/Spot.js
@@ -0,0 +1,132 @@
+const Spot = () => {
+  return (
+    <svg
+      width="50"
+      height="46"
+      viewBox="0 0 50 46"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M49.1371 7.04737L45.2345 3.14472C44.085 1.99522 42.2213 1.99522 41.075 3.14472L36.3987 7.82101V23.9516L49.1402 11.21C50.2866 10.0636 50.2866 8.2 49.1402 7.0505L49.1371 7.04737ZM45.2908 12.6351L39.6467 6.99099L40.7586 5.87908L46.4027 11.5232L45.2908 12.6351Z"
+        fill="url(#paint0_linear_242_73)"
+      />
+      <path
+        d="M30.695 0.00313162H5.70364C2.55896 0.00313162 0 2.56209 0 5.70677V39.6185C0 42.7632 2.55896 45.3221 5.70364 45.3221H30.695C33.8397 45.3221 36.3987 42.7632 36.3987 39.6185V5.70364C36.3987 2.55896 33.8397 0 30.695 0V0.00313162ZM35.9539 39.6185C35.9539 42.522 33.5985 44.8774 30.695 44.8774H5.70364C2.80014 44.8774 0.444765 42.522 0.444765 39.6185V5.70364C0.444765 2.80014 2.80014 0.444765 5.70364 0.444765H30.695C33.5985 0.444765 35.9539 2.80014 35.9539 5.70364V39.6154V39.6185Z"
+        fill="white"
+      />
+      <path
+        style={{ mixBlendMode: "overlay" }}
+        d="M30.695 0.444824H5.70363C2.79924 0.444824 0.444763 2.7993 0.444763 5.70369V39.6154C0.444763 42.5198 2.79924 44.8743 5.70363 44.8743H30.695C33.5994 44.8743 35.9539 42.5198 35.9539 39.6154V5.70369C35.9539 2.7993 33.5994 0.444824 30.695 0.444824Z"
+        fill="url(#paint1_linear_242_73)"
+      />
+      <path
+        d="M28.8972 10.1952H8.39102C7.88048 10.1952 7.46704 10.6086 7.46704 11.1192C7.46704 11.6297 7.88048 12.0432 8.39102 12.0432H28.8972C29.4077 12.0432 29.8212 11.6297 29.8212 11.1192C29.8212 10.6086 29.4077 10.1952 28.8972 10.1952Z"
+        fill="url(#paint2_linear_242_73)"
+      />
+      <path
+        d="M28.8972 35.7096H8.39102C7.88048 35.7096 7.46704 36.123 7.46704 36.6336C7.46704 37.1441 7.88048 37.5576 8.39102 37.5576H28.8972C29.4077 37.5576 29.8212 37.1441 29.8212 36.6336C29.8212 36.123 29.4077 35.7096 28.8972 35.7096Z"
+        fill="url(#paint3_linear_242_73)"
+      />
+      <path
+        d="M16.2308 23.1027H8.39102C7.88048 23.1027 7.46704 23.5161 7.46704 24.0266C7.46704 24.5372 7.88048 24.9506 8.39102 24.9506H16.2308C16.7413 24.9506 17.1548 24.5372 17.1548 24.0266C17.1548 23.5161 16.7413 23.1027 16.2308 23.1027Z"
+        fill="url(#paint4_linear_242_73)"
+      />
+      <path
+        d="M20.5751 16.5283H8.39102C7.88048 16.5283 7.46704 16.9418 7.46704 17.4523C7.46704 17.9628 7.88048 18.3763 8.39102 18.3763H20.5751C21.0856 18.3763 21.4991 17.9628 21.4991 17.4523C21.4991 16.9418 21.0856 16.5283 20.5751 16.5283Z"
+        fill="url(#paint5_linear_242_73)"
+      />
+      <path
+        d="M15.6482 29.3169H8.39102C7.88048 29.3169 7.46704 29.7303 7.46704 30.2409C7.46704 30.7514 7.88048 31.1649 8.39102 31.1649H15.6482C16.1587 31.1649 16.5722 30.7514 16.5722 30.2409C16.5722 29.7303 16.1587 29.3169 15.6482 29.3169Z"
+        fill="url(#paint6_linear_242_73)"
+      />
+      <defs>
+        <linearGradient
+          id="paint0_linear_242_73"
+          x1="37.5576"
+          y1="37.592"
+          x2="44.37"
+          y2="2.60285"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#5625CA" />
+          <stop offset="0.47" stopColor="#3F8DE5" />
+          <stop offset="1" stopColor="#11EFC4" />
+        </linearGradient>
+        <linearGradient
+          id="paint1_linear_242_73"
+          x1="28.4242"
+          y1="32.8844"
+          x2="-0.119023"
+          y2="4.34121"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="white" stopOpacity="0" />
+          <stop offset="1" stopColor="white" />
+        </linearGradient>
+        <linearGradient
+          id="paint2_linear_242_73"
+          x1="39.2239"
+          y1="31.6974"
+          x2="16.3247"
+          y2="8.80139"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#5625CA" />
+          <stop offset="0.47" stopColor="#3F8DE5" />
+          <stop offset="1" stopColor="#11EFC4" />
+        </linearGradient>
+        <linearGradient
+          id="paint3_linear_242_73"
+          x1="26.4666"
+          y1="44.4545"
+          x2="3.57068"
+          y2="21.5586"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#5625CA" />
+          <stop offset="0.47" stopColor="#3F8DE5" />
+          <stop offset="1" stopColor="#11EFC4" />
+        </linearGradient>
+        <linearGradient
+          id="paint4_linear_242_73"
+          x1="29.6019"
+          y1="41.3192"
+          x2="6.70593"
+          y2="18.4232"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#5625CA" />
+          <stop offset="0.47" stopColor="#3F8DE5" />
+          <stop offset="1" stopColor="#11EFC4" />
+        </linearGradient>
+        <linearGradient
+          id="paint5_linear_242_73"
+          x1="33.9744"
+          y1="36.9468"
+          x2="11.0784"
+          y2="14.0508"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#5625CA" />
+          <stop offset="0.47" stopColor="#3F8DE5" />
+          <stop offset="1" stopColor="#11EFC4" />
+        </linearGradient>
+        <linearGradient
+          id="paint6_linear_242_73"
+          x1="26.3507"
+          y1="44.5704"
+          x2="3.45479"
+          y2="21.6744"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#5625CA" />
+          <stop offset="0.47" stopColor="#3F8DE5" />
+          <stop offset="1" stopColor="#11EFC4" />
+        </linearGradient>
+      </defs>
+    </svg>
+  );
+};
+
+export default Spot;
--- src/pages/AddJournal/index.js
+++ src/pages/AddJournal/index.js
@@ -1,6 +1,14 @@
+import { useEffect, useState } from "react";
 import PairInputs from "src/components/formActions/PairInputs";
-
+import Feature from "src/components/icons/Feature";
+import Spot from "src/components/icons/Spot";
+const plans = [
+  { name: "Feature", icon: () => Feature(), selected: false },
+  { name: "spot", icon: () => Spot(), selected: false },
+];
 const AddJournal = () => {
+  const [selectPlan, setSelectPlan] = useState(-1);
+
   return (
     <>
       <div className="max-w-screen-sm px-4 mx-auto">
@@ -15,24 +23,27 @@ const AddJournal = () => {
             </h2>
           </div>
         </div>
-        <div className="flex flex-col gap-y-2 mt-10">
+        <div className="flex flex-col gap-y-2 mt-10 ">
           <h1 className="text-slate-800  font-bold">Pair</h1>
-          <div className="flex items-center gap-x-4">
+          <div className="flex items-center w-full gap-x-2 ">
             <PairInputs name={"from"} />
             <PairInputs name={"to"} />
           </div>
         </div>
-        <div className="flex flex-col gap-y-2 mt-10">
+        <div className="flex flex-col gap-y-2 mt-10 ">
           <h1 className="text-slate-800  font-bold"> Transaction type</h1>
-          <div className="flex items-center gap-x-4">
-            <div className="py-4 px-2 rounded-lg bg-violet-100 flex flex-col items-center flex-1">
-                <img src="/images/icons/Feature.svg" />
-                <h1 className="text-slate-800 text-sm font-extrabold">Feature</h1>
-            </div>
-            <div className="py-4 px-2 rounded-lg bg-violet-100 flex flex-col items-center flex-1">
-                <img src="/images/icons/Feature.svg" />
-                <h1 className="text-slate-800 text-sm font-extrabold">Feature</h1>
-            </div>
+          <div className="flex items-center gap-x-4 px-2 ">
+            {plans.map((plan, i) => {
+              return (
+                <TradePlan
+                  plan={plan}
+                  i={i}
+                  selectPlan={selectPlan}
+                  setSelectPlan={setSelectPlan}
+                  key={i}
+                />
+              );
+            })}
           </div>
         </div>
       </div>
@@ -41,3 +52,19 @@ const AddJournal = () => {
 };
 
 export default AddJournal;
+
+const TradePlan = ({ plan, i, selectPlan, setSelectPlan }) => {
+  return (
+    <button
+      onClick={() => setSelectPlan(i)}
+      className={`py-4 rounded-lg ${
+        selectPlan === i
+          ? "bg-violet-200 ring-2 ring-violet-400"
+          : "bg-violet-100"
+      } transition-all ease-in-out duration-300 flex flex-col justify-between items-center flex-1`}
+    >
+      <span>{plan.icon()}</span>
+      <h1 className="text-slate-800 pt-2 text-sm font-bold">{plan.name}</h1>
+    </button>
+  );
+};

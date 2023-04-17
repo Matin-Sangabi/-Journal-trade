5a43e24aaf38f5b318c54af50bd9daaf33f92387
Matin Sangabi <sangabiwork@gmail.com>
2023-04-17 15:13:02 +0430

    hidden app building in mobile design

--- src/container/layout/index.js
+++ src/container/layout/index.js
@@ -10,7 +10,7 @@ const Layout = ({ children }) => {
       <div className="max-w-screen-xl px-4 mt-4 overflow-hidden mb-40 md:hidden ">
         {children}
       </div>
-      <div className="w-full flex  items-center mt-8 justify-center">
+      <div className="w-full hidden md:flex  items-center mt-8 justify-center">
         <div className="flex flex-col">
           <h1 className="text-4xl font-bold text-slate-800">App Development</h1>
           <h1 className="text-slate-500 text-2xl font-semibold">
